5424013c9b6c3ba3d76ba4c35b8654246c50f82d
Randark <koldaev.alexey@gmail.com>
2022-07-01 23:21:24 +0300

    learn if..else and for

--- js/script.js
+++ js/script.js
@@ -1,32 +1,6 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
 "use strict";
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
-const lastFilmName1 = prompt("Один из последних просмотренных фильмов?");
-const lastFilmScore1 = prompt("На сколько оцените его?");
-const lastFilmName2 = prompt("Один из последних просмотренных фильмов?");
-const lastFilmScore2 = prompt("На сколько оцените его?");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -36,7 +10,30 @@ const personalMovieDB = {
   privat: false,
 };
 
-personalMovieDB.movies[lastFilmName1] = lastFilmScore1;
-personalMovieDB.movies[lastFilmName2] = lastFilmScore2;
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else alert("Вы киноман");
+
+invalidInputValue: for (let i = 0; i < 2; i++) {
+  let lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+  if (lastMovie == "" || lastMovie === null || lastMovie.length > 50) {
+    alert("Ошибка!");
+    i--;
+    continue invalidInputValue;
+  }
+  let lastMovieScore = prompt("На сколько оцените его?", "");
+  if (
+    lastMovieScore == "" ||
+    lastMovieScore === null ||
+    lastMovieScore.length > 50
+  ) {
+    alert("Ошибка!");
+    i--;
+    continue invalidInputValue;
+  }
+  personalMovieDB.movies[lastMovie] = lastMovieScore;
+}
 
 console.log(personalMovieDB);
